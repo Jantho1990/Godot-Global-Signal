568f82d1833ba999e452f9f3a573b4bc49d80a05
hazarek <duurkak@yandex.com>
2022-02-13 19:01:20 +0300

    indentation fix

--- TestLabel.gd
+++ TestLabel.gd
@@ -3,8 +3,8 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  GlobalSignal.add_listener('text_updated', self, '_on_Text_updated')
+	GlobalSignal.add_listener("text_updated", self, "_on_Text_updated")
 
 
 func _on_Text_updated(text_value: String):
-  text = text_value
+	text = text_value
--- TestTextEdit.gd
+++ TestTextEdit.gd
@@ -5,10 +5,10 @@ signal text_updated(text_value)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-  GlobalSignal.add_emitter('text_updated', self)
-  GlobalSignal.emit_signal_when_ready('text_updated', ['text in _ready()'], self)
-  connect('text_changed', self, '_on_Text_changed')
+	GlobalSignal.add_emitter("text_updated", self)
+	GlobalSignal.emit_signal_when_ready("text_updated", ["text in _ready()"], self)
+	connect("text_changed", self, "_on_Text_changed")
 
 
 func _on_Text_changed(_value):
-  emit_signal('text_updated', text)
+	emit_signal("text_updated", text)
